87fa2131654a8d11cd63278cb6fe60ffed31219b
ftmhabt <ftmh.a78@gmail.com>
2025-09-02 23:47:21 +0330

    Extract phone from the url

--- src/app/api/user/[phone]/route.ts
+++ src/app/api/user/[phone]/route.ts
@@ -1,22 +1,27 @@
 import { db } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { phone: string } }
-) {
-  const { phone } = params;
+export async function GET(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const phone = pathname.split("/").pop();
+
+  if (!phone) {
+    return NextResponse.json({ error: "Phone not provided" }, { status: 400 });
+  }
+
   const user = await db.user.findUnique({
     where: { phone },
     select: {
       phone: true,
       password: false,
-      // assuming full name is stored as firstName and lastName in address for now
       Address: true,
     },
   });
-  if (!user)
+
+  if (!user) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
   return NextResponse.json(user);
 }
 

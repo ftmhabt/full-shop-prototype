534c78c324abb9ec84f3c72eeed3f714fdba7351
ftmhabt <ftmh.a78@gmail.com>
2025-09-05 21:53:32 +0330

    Limit access of other roles

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,14 +6,14 @@ const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
 
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
+  const token = req.cookies.get("token")?.value;
+  const url = req.nextUrl.pathname;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
   if (pathname.startsWith("/dashboard")) {
-    const token = req.cookies.get("token")?.value;
-
-    if (!token) {
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
-
     try {
       await jwtVerify(token, secret);
       return NextResponse.next();
@@ -21,10 +21,22 @@ export async function middleware(req: NextRequest) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
   }
+  const role = token ? JSON.parse(atob(token.split(".")[1])).role : null;
 
+  if (url.startsWith("/admin") && role !== "admin") {
+    return NextResponse.redirect(new URL("/auth/login", req.url));
+  }
+
+  if (url.startsWith("/writer") && role !== "writer" && role !== "admin") {
+    return NextResponse.redirect(new URL("/auth/login", req.url));
+  }
+
+  if (url.startsWith("/dashboard") && role !== "user") {
+    return NextResponse.redirect(new URL("/auth/login", req.url));
+  }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/admin/:path*", "/writer/:path*", "/dashboard/:path*"],
 };

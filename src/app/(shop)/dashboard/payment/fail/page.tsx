f138b3a5367f26b51e4d0efdf469a525801963a2
ftmhabt <ftmh.a78@gmail.com>
2025-09-12 18:30:19 +0330

    Remove unnecessary suspenses

--- src/app/(shop)/dashboard/payment/fail/page.tsx
+++ src/app/(shop)/dashboard/payment/fail/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import PaymentFailClient from "@/components/payment/PaymentFailClient";
-import { Suspense } from "react";
 
 export default function PaymentFailPage() {
-  return (
-    <Suspense fallback={<p>در حال بارگذاری...</p>}>
-      <PaymentFailClient />
-    </Suspense>
-  );
+  return <PaymentFailClient />;
 }
--- src/app/(shop)/dashboard/payment/success/page.tsx
+++ src/app/(shop)/dashboard/payment/success/page.tsx
@@ -1,10 +1,5 @@
 import PaymentSuccessClient from "@/components/payment/PaymentSuccessClient";
-import { Suspense } from "react";
 
 export default function PaymentSuccessPage() {
-  return (
-    <Suspense fallback={<p>در حال بارگذاری...</p>}>
-      <PaymentSuccessClient />
-    </Suspense>
-  );
+  return <PaymentSuccessClient />;
 }
